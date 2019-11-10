830b9970fa883f1f144f5de106c6207280d7eb02
Inma_Burlo <inmaburlo@gmail.com>
2019-11-10 18:15:13 +0100

    Cambios en formula para generar numero aleatorio en rango 10-50

--- src/proyectot4entornos/Aciertos.java
+++ src/proyectot4entornos/Aciertos.java
@@ -4,7 +4,7 @@ public class Aciertos {
 
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+        int x = (int) Math.floor(Math.random()*(50-10+1)+10);
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
@@ -17,7 +17,7 @@ public class Aciertos {
             }
             cont++;
         } while (n != x);
-        System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
+        System.out.println("Excelente!!! Haz acertado , No. de intentos: "+cont );
         sc.close();
     }
 }
